fd73641e25059e1a449ae6d1b12df7096219c464
Arsentij Buyakov <aobuyakov@gmail.com>
2025-05-24 15:07:48 +0300

    Project sprint 5

--- test/manager/InMemoryTaskManagerTest.java
+++ test/manager/InMemoryTaskManagerTest.java
@@ -1,10 +1,21 @@
 package manager;
 
+import model.Subtask;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import util.Managers;
+import util.TaskStatus;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryTaskManagerTest {
+
+    private static TaskManager taskManager;
+
+    @BeforeEach
+    void setUp() {
+        taskManager = Managers.getDefault();
+    }
 
 
 }
